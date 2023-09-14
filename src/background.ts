d0fc68e7ef028eed3dc6d8996144b26850ceffc4
Lasse Nørfeldt <lasse@norfeldt.com>
2023-09-14 12:32:44 +0200

    ✨ refactor to use react instead

--- src/background.ts
+++ src/background.ts
@@ -1,22 +1,53 @@
-// Clicking the extension icon
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id! },
-    files: ['content.js'],
-  })
+let loadedTabs: number[] = []
+
+chrome.runtime.onMessage.addListener((message, sender) => {
+  if (message.action === 'contentScriptLoaded') {
+    loadedTabs.push(sender.tab?.id!)
+  }
 })
 
-// Create a context menu item
+function showContent(tab: chrome.tabs.Tab | undefined) {
+  setTimeout(() => {
+    chrome.tabs.sendMessage(tab?.id!, { action: 'showContent' })
+  }, 100) // wait for 100ms before sending the message
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  if (loadedTabs.includes(tab.id!)) {
+    // Content script is already present, just send the message
+    showContent(tab)
+  } else {
+    // Inject the content script
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tab.id! },
+        files: ['content.js'],
+      },
+      () => showContent(tab)
+    )
+  }
+})
+
 chrome.contextMenus.create({
   id: 'searchMaps',
   title: "Search Maps for '%s'",
   contexts: ['selection'],
 })
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === 'searchMaps') {
-    chrome.scripting.executeScript({
-      target: { tabId: tab?.id! },
-      files: ['content.js'],
-    })
+    if (loadedTabs.includes(tab?.id!)) {
+      // Content script is already present, just send the message
+      showContent(tab)
+    } else {
+      // Inject the content script
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tab?.id! },
+          files: ['content.js'],
+        },
+        () => showContent(tab)
+      )
+    }
   }
 })
